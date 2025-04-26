9d97d2a29ead98b56a909d4b767a193dd22a7f64
Bima Jatmiko Abadi <bja.devs@gmail.com>
2025-04-26 21:59:30 +0700

    Add express

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,15 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
 import { onRequest } from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+import express from "express";
 
-export const api = onRequest((_, response) => {
+const app = express();
+
+app.use(express.json());
+
+app.get("/ping", (_, response) => {
     logger.info("Hello logs!", { structuredData: true });
-    response.send("Hello from Firebase API!");
+    response.send("Hello from Firebase API");
 });
+
+export const api = onRequest(app);
